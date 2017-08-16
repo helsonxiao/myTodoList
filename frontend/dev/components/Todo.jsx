19d78e97840e471dec81d52c95151f00c7159261
helsonxiao <helsonxiao@gmail.com>
2017-08-16 21:56:22 +0800

    show todo detail - bug is not fixed

---
+++ frontend/dev/components/Todo.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default class Todo extends React.Component{
+    constructor(props) {
+        super(props);
+        this.toggleDone = this.toggleDone.bind(this);
+    }
+    
+    toggleDone(todo) {
+        todo.done = !todo.done;
+        this.props.edit(todo);
+    }
+    
+    render() {
+        var todo = this.props.todo;
+        if (todo.done) {
+            var toggle = 'Mark as not done';
+        } else {
+            var toggle = 'Mark as done';
+        }
+        
+        return (
+            <div>
+                <h3>{todo.title}</h3>
+                <p>{todo.text}</p>
+                <button 
+                    onClick={ () => this.toggleDone(todo) }>
+                    {toggle}</button>
+            </div>
+        );
+    }
+}
