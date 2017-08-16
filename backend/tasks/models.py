3d314905164bc08c0279b22f68702f41da8aa513
helsonxiao <helsonxiao@gmail.com>
2017-08-16 16:53:29 +0800

    add priority validation, hide todo's birthday, make deadline nullable

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,13 +1,18 @@
 from django.db import models
-from django.utils import timezone
+from django.core.exceptions import ValidationError
+
+
+def validate_priority(value):
+    if value not in ['1', '2', '3']:
+        raise ValidationError("Only three levels for priority. They are 1, 2, 3.")
 
 
 # Create your models here.
 class Task(models.Model):
     title = models.CharField(max_length=120)
-    birthday = models.DateField(default=timezone.now, blank=True)
-    deadline = models.DateField(default=timezone.now)
-    priority = models.IntegerField(default=0)
+    birthday = models.DateField(auto_now_add=True)
+    deadline = models.DateField(blank=True, null=True)
+    priority = models.IntegerField(default=0, validators=[validate_priority])
     text = models.TextField()
     done = models.BooleanField(default=False, blank=True)
     
