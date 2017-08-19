b04b1b6fc287c72634aea51cebef536b0d25c674
helsonxiao <helsonxiao@gmail.com>
2017-08-19 22:38:28 +0800

    cancel pagination temporarily

--- backend/todolist/settings.py
+++ backend/todolist/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = (
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
-    'PAGE_SIZE': 5
+    # 'PAGE_SIZE': 5
 }
 
 MIDDLEWARE_CLASSES = (
