7ba360f25fe5b2c27229ae98c3342d26af73f8da
helsonxiao <helsonxiao@gmail.com>
2017-08-20 11:03:50 +0800

    priority field integer => char

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -2,17 +2,20 @@ from django.db import models
 from django.core.exceptions import ValidationError
 
 
-def validate_priority(value):
-    if value not in [1, 2, 3]:
-        raise ValidationError("Only three levels for priority. They are 1, 2, 3.")
-
-
 # Create your models here.
 class Task(models.Model):
+    LOW = '1'
+    MEDIUM = '2'
+    HIGH = '3'
+    PRIORITY_CHOICES = (
+        (LOW, 'Low'),
+        (MEDIUM, 'Medium'),
+        (HIGH, 'High'),
+    )
+    priority = models.CharField(choices=PRIORITY_CHOICES, default=LOW, max_length=100)
     title = models.CharField(max_length=120)
     birthday = models.DateField(auto_now_add=True)
     deadline = models.DateField(blank=True, null=True)
-    priority = models.IntegerField(default=0, validators=[validate_priority])
     text = models.TextField()
     done = models.BooleanField(default=False, blank=True)
     
