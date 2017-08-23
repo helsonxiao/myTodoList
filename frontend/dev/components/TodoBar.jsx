adc2236b2c8ae3f349628472e85c685710eaa6ac
helsonxiao <helsonxiao@gmail.com>
2017-08-23 14:50:37 +0800

    add 2^2 sort methods

--- frontend/dev/components/TodoBar.jsx
+++ frontend/dev/components/TodoBar.jsx
@@ -4,6 +4,16 @@ import { observer } from 'mobx-react';
 @observer
 export default class TodoBar extends React.Component{
     render() {
-        return <button onClick={this.props.handleAddClick}>Add</button>
+        return (
+            <div>
+                <button onClick={this.props.handleAddClick}>Add</button>
+                <label>优先级：</label>
+                <button onClick={this.props.handlePriUp}>低的</button>
+                <button onClick={this.props.handlePriDown}>高的</button>
+                <label>DDL：</label>
+                <button onClick={this.props.handleDDLUp}>近的</button>
+                <button onClick={this.props.handleDDLDown}>远的</button>
+            </div>
+        );
     }
 }
--- frontend/dev/containers/TodoBarContainer.jsx
+++ frontend/dev/containers/TodoBarContainer.jsx
@@ -7,18 +7,74 @@ export default class TodoBarContainer extends React.Component{
     constructor(props){
         super(props);
         this.handleAddClick = this.handleAddClick.bind(this);
+        this.handlePriUp = this.handlePriUp.bind(this);
+        this.handlePriDown = this.handlePriDown.bind(this);
+        this.handleDDLUp = this.handleDDLUp.bind(this);
+        this.handleDDLDown = this.handleDDLDown.bind(this);
     }
 
     handleAddClick() {
         this.props.appState.isAdding = true;
-//        this.props.appState.isOpening = false;
+        this.props.appState.isOpening = false;
+    }
+
+    handlePriUp() {
+        this.props.appState.residue = this.props.appState.residue.sort((x, y) => {
+            if (x.priority < y.priority) {
+                return -1;
+            }
+            if (x.priority > y.priority) {
+                return 1;
+            }
+            return 0;
+        });
+    }
+
+    handlePriDown() {
+        this.props.appState.residue = this.props.appState.residue.sort((x, y) => {
+            if (x.priority < y.priority) {
+                return 1;
+            }
+            if (x.priority > y.priority) {
+                return -1;
+            }
+            return 0;
+        });
+    }
+
+    handleDDLUp() {
+        this.props.appState.residue = this.props.appState.residue.sort((x, y) => {
+            if (x.deadline < y.deadline) {
+                return -1;
+            }
+            if (x.deadline > y.deadline) {
+                return 1;
+            }
+            return 0;
+        });
+    }
+
+    handleDDLDown() {
+        this.props.appState.residue = this.props.appState.residue.sort((x, y) => {
+            if (x.deadline < y.deadline) {
+                return 1;
+            }
+            if (x.deadline > y.deadline) {
+                return -1;
+            }
+            return 0;
+        });
     }
 
     render() {
         return (
             <TodoBar
                 appState={this.props.appState}
-                handleAddClick={this.handleAddClick}/>
+                handleAddClick={this.handleAddClick}
+                handlePriUp={this.handlePriUp}
+                handlePriDown={this.handlePriDown}
+                handleDDLUp={this.handleDDLUp}
+                handleDDLDown={this.handleDDLDown}/>
         );
     }
 }
